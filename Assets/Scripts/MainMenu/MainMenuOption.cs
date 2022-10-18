04ef252505ac54c4166aacc854254b7eb0b7dca7
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-10-18 12:46:44 -0400

    Doesn't work as before. It seems like there's something wrong with post process

--- Assets/Scripts/MainMenu/MainMenuOption.cs
+++ Assets/Scripts/MainMenu/MainMenuOption.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class MainMenuOption : MonoBehaviour
@@ -13,15 +14,33 @@ public class MainMenuOption : MonoBehaviour
     [Header("Brightness")]
     public Slider brightnessSlider;
     public TextMeshProUGUI brightnessSliderNumber;
-    
+    public PostProcessProfile brightness;
+    public PostProcessLayer layer;
+    AutoExposure exposure;
+
 
     private void Start()
     {
+        brightness.TryGetSettings(out exposure);
         AdjustVolume(volumeSlider.value);
+        AdjustVolume(brightnessSlider.value);
     }
     public void AdjustVolume(float value)
     {
         volumeSliderNumber.text = (value + 80).ToString();
         audioMixer.SetFloat("volume", value);
+    }
+
+    public void AdjustBrightness(float value)
+    {
+        if(value!=0)
+        {
+            exposure.keyValue.value = value;
+        }
+        else
+        {
+            exposure.keyValue.value = .05f;
+
+        }
     }
 }
