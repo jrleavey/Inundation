87ac200c53818849947ca4d2d3abf25b1b967b7f
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-10-18 12:11:24 -0400

    Push before redownload Post Process package

--- Assets/Scripts/MainMenu/MainMenuOption.cs
+++ Assets/Scripts/MainMenu/MainMenuOption.cs
@@ -1,7 +1,6 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class MainMenuOption : MonoBehaviour
@@ -14,33 +13,15 @@ public class MainMenuOption : MonoBehaviour
     [Header("Brightness")]
     public Slider brightnessSlider;
     public TextMeshProUGUI brightnessSliderNumber;
-    public PostProcessProfile brightness;
-    public PostProcessLayer layer;
-    AutoExposure exposure;
+    
 
     private void Start()
     {
-        brightness.TryGetSettings(out exposure);
-        AdjustBrightness(brightnessSlider.value);
         AdjustVolume(volumeSlider.value);
     }
     public void AdjustVolume(float value)
     {
         volumeSliderNumber.text = (value + 80).ToString();
         audioMixer.SetFloat("volume", value);
-    }
-
-    public void AdjustBrightness(float value)
-    {
-        brightnessSliderNumber.text = value.ToString();
-
-        if (value != 0)
-        {
-            exposure.keyValue.value = value;
-        }
-        else
-        {
-            exposure.keyValue.value = .05f;
-        }
     }
 }
