49cbd9bc1438bd57560c345d1eecb4e68ef68b02
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-10-25 21:15:09 -0400

    make the playercontroller script public and push it

--- Assets/Scripts/ParticleSystem/Particles.cs
+++ Assets/Scripts/ParticleSystem/Particles.cs
@@ -2,68 +2,26 @@ using UnityEngine;
 
 public class Particles : MonoBehaviour
 {
-    //ParticleEventSystem shotgunffect
-    //ParticleEventSystem smgEffect
-    //ParticleEventSystem pistolEffect
-    //ParticleEventSystem arEffect
+    [SerializeField] ParticleSystem shotgunffect;
+    [SerializeField] ParticleSystem smgEffect;
+    [SerializeField] ParticleSystem pistolEffect;
+    [SerializeField] ParticleSystem arEffect;
+    PlayerController playerController;
+    float hiteffectAfterAfewSecond;
 
-
-    //Particle
-    //float hiteffectAfterAfewSecond
-    //
-    void Start()
+    private void Start()
     {
-        ParticleEventSystem.current.muzzleEvent += PlayMuzzleEvent;
-        ParticleEventSystem.current.enemyHitEvent += PlayEnemyHitEffect;
+        playerController = FindObjectOfType<PlayerController>();
     }
 
-    void PlayMuzzleEvent()
+
+    public void PlayMuzzleFlash()
     {
-        //GameObject impact;
-        //switch(weaponID)
-        //{
-        //case: 0
-        //playmuzzleFlasheffect.Play();
-        //impact=Instantiate(shoutgunHiteffect, hit.point, QuaternionLook(hit.normal));
-        //Destroy(impact, 0.1f);
-        //Play shotgun smoke effect
+        switch (playerController.)
+        {
+            case _ActiveWeapon. 
+        
+        }
 
-        //case: 1
-        //playmuzzleFlasheffect.Play();
-        //impact=Instantiate(pistolHitEffect, hit.point, QuaternionLook(hit.normal));
-        //Destroy(impact, 0.1f);
-
-        //Play pistol smoke effect
-
-
-
-        //case: 2
-        //playmuzzleFlasheffect.Play();
-        //impact=Instantiate(smgHitEffect, hit.point, QuaternionLook(hit.normal));
-        //Destroy(impact, 0.1f);
-        //Play smg smoke effect
-
-
-
-        //case: 3
-        //playmuzzleFlasheffect.Play();
-        //impact=Instantiate(ARHitEffect, hit.point, QuaternionLook(hit.normal));
-        //Destroy(impact, 0.1f);
-        //Play AR smoke effect
-
-
-        //default: print(something's wrong);
-        //}
     }
-
-    void PlayEnemyHitEffect()
-    {
-        //StartCoroutine(hiteffectAfterAfewSecond)
-    }
-
-    //void IEnumerator(float hiteffectAfterAfewSecond)
-    //new WaitForSecond(hiteffectAfterAfewSecond)
-    //EnemyHitEffect.Play();
-
-
 }
