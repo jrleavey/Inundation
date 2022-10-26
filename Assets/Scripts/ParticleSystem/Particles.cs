723a648e67432e6aa65b3ae37d4b589a9fbb2d49
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-10-25 23:51:16 -0400

    Import MuzzleFlashSmoke Assets

--- Assets/Scripts/ParticleSystem/Particles.cs
+++ Assets/Scripts/ParticleSystem/Particles.cs
@@ -1,23 +1,54 @@
+using System.Collections;
 using UnityEngine;
+using static PlayerController;
 
 public class Particles : MonoBehaviour
 {
+    public enum WeaponType
+    {
+        Handgun,
+        Shotgun,
+        SMG,
+        AR
+    }
+
+   
+    [SerializeField] WeaponType weaponType;
     [SerializeField] ParticleSystem shotgunffect;
     [SerializeField] ParticleSystem smgEffect;
-    [SerializeField] ParticleSystem pistolEffect;
-    [SerializeField] ParticleSystem arEffect;
-    PlayerController playerController;
-    float hiteffectAfterAfewSecond;
+    [SerializeField] ParticleSystem handgunEffect;
+    [SerializeField] ParticleSystem rifleEffect;
+    [SerializeField] float hiteffectAfterAfewSecond;
 
-    private void Start()
+    private void Update()
     {
-        playerController = FindObjectOfType<PlayerController>();
+        PlayMuzzleFlash(weaponType);
+    }
+    public void PlayMuzzleFlash(WeaponType weaponType)
+    {
+        switch (weaponType)
+        {
+            case WeaponType.Handgun:
+                handgunEffect.Play();
+                break;
+
+            case WeaponType.Shotgun:
+                shotgunffect.Play();
+                break;
+
+            case WeaponType.SMG:
+                smgEffect.Play();
+                break;
+
+            case WeaponType.AR:
+                rifleEffect.Play();
+                break;
+
+            default:
+                Debug.Log("Error");
+                break;
+        }
     }
 
-
-    public void PlayMuzzleFlash()
-    {
-        
-
-    }
+   
 }
