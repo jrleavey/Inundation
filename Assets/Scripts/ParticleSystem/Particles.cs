83a3087cdaef6703a029907d8d1dcd6edf852712
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-10-27 22:33:27 -0400

    Updated Particle script for blood flies

--- Assets/Scripts/ParticleSystem/Particles.cs
+++ Assets/Scripts/ParticleSystem/Particles.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using static PlayerController;
 
@@ -7,6 +8,8 @@ public class Particles : MonoBehaviour
     [SerializeField] ParticleSystem smgEffect;
     [SerializeField] ParticleSystem handgunEffect;
     [SerializeField] ParticleSystem rifleEffect;
+    [SerializeField] GameObject hitEffect;
+    [SerializeField] LayerMask enemyLayerMask;
 
     public void PlayMuzzleFlash(ActiveWeapon weaponType)
     {
@@ -32,5 +35,20 @@ public class Particles : MonoBehaviour
                 Debug.Log("Error");
                 break;
         }
+    }
+
+    public void HitEffectOnEnemies()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit, 100f, enemyLayerMask))
+        {
+            CreateHitImpact(hit);
+        }
+    }
+
+    public void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact =Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 0.5f);
     }
 }
