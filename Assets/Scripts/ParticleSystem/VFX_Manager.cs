ed82437cc79938f1dcc0163a46562ef557c534ce
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-11-05 18:29:33 -0400

    Made Simple light prefab
    Made Simple light prefab

---
+++ Assets/Scripts/ParticleSystem/VFX_Manager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class VFX_Manager : MonoBehaviour
+{
+    //VFX List:
+    //Green
+    //Blue
+    //Red
+    //White
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
